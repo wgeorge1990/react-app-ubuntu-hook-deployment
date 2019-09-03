624f160cadac9a6d90d1c266084320e8a16f9787
wgeorge1990 <williameverettgeorge@icloud.com>
2019-09-03 09:54:59 -0400

    added text body to app div to test if nginx is serving properly

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      Welcome to my react app being served on Digital Ocean and being redployed using webhooks written in GO.
     </div>
   );
 }
