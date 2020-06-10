ff2b8f736c6695d1d29623f661d24243d308be80
wgeorge1990 <williameverettgeorge@icloud.com>
2020-06-10 17:31:39 -0400

    take down site for changes

--- src/App.js
+++ src/App.js
@@ -41,156 +41,159 @@ class App extends Component {
   render() {
     const { animation, dimmed, direction, visible } = this.state
     return (
-      <Container style={{ 'width': '100%' }}>
-        <Menu fluid widths={3}>
-          <Menu.Item onClick={this.toggleMenu}>
-            <Icon name='bars' />
-          </Menu.Item>
-          <Menu.Item onClick={this.back}>
-            <Icon name='arrow circle left' />
-          </Menu.Item>
-          <Menu.Item onClick={this.forward}>
-            <Icon name='arrow circle right' />
-          </Menu.Item>
-        </Menu>
+      <Container style={{textAlign: "center", paddingTop: 300}}>
+        <h1>Site Under Construction </h1>
+      </Container>
+      // <Container style={{ 'width': '100%' }}>
+      //   <Menu fluid widths={3}>
+      //     <Menu.Item onClick={this.toggleMenu}>
+      //       <Icon name='bars' />
+      //     </Menu.Item>
+      //     <Menu.Item onClick={this.back}>
+      //       <Icon name='arrow circle left' />
+      //     </Menu.Item>
+      //     <Menu.Item onClick={this.forward}>
+      //       <Icon name='arrow circle right' />
+      //     </Menu.Item>
+      //   </Menu>
         
 
-        <Sidebar.Pushable as={Container} fluid>
-          <Sidebar
-            // onMouseLeave={this.toggleMenu}
-            style={{ "padding": "10px" }}
-            as={Menu}
-            animation={animation }
-            direction={direction}
-            icon='labeled'
-            inverted
-            vertical
-            visible={visible}
-            size={this.state.sidebarSize} >
+      //   <Sidebar.Pushable as={Container} fluid>
+      //     <Sidebar
+      //       // onMouseLeave={this.toggleMenu}
+      //       style={{ "padding": "10px" }}
+      //       as={Menu}
+      //       animation={animation }
+      //       direction={direction}
+      //       icon='labeled'
+      //       inverted
+      //       vertical
+      //       visible={visible}
+      //       size={this.state.sidebarSize} >
             
-            <Menu.Item>
-              <Link to="/" >
-                <Image
-                  centered
-                  circular
-                  size='mini'
-                  src={require('./images/profile/IMG_1327.JPG')}
-                      onClick={this.toggleMenu} />
-              </Link>
-            </Menu.Item>
+      //       <Menu.Item>
+      //         <Link to="/" >
+      //           <Image
+      //             centered
+      //             circular
+      //             size='mini'
+      //             src={require('./images/profile/IMG_1327.JPG')}
+      //                 onClick={this.toggleMenu} />
+      //         </Link>
+      //       </Menu.Item>
 
 
-            <Link
-              to='/FineArt'
-              className="item"
-              onClick={this.toggleMenu}>
-              <i className="paint brush icon"></i>
-              FineArt
-            </Link>
+      //       <Link
+      //         to='/FineArt'
+      //         className="item"
+      //         onClick={this.toggleMenu}>
+      //         <i className="paint brush icon"></i>
+      //         FineArt
+      //       </Link>
 
-            <Link
-              to="/Development"
-              className="item"
-              onClick={this.toggleMenu} >
-              <i className="computer icon"></i>
-              Development
-            </Link>
+      //       <Link
+      //         to="/Development"
+      //         className="item"
+      //         onClick={this.toggleMenu} >
+      //         <i className="computer icon"></i>
+      //         Development
+      //       </Link>
   
-            <Link
-              to="/Resume"
-              className="item"
-              onClick={this.toggleMenu} >
-              <i className="pencil icon"></i>
-              Resume
+      //       <Link
+      //         to="/Resume"
+      //         className="item"
+      //         onClick={this.toggleMenu} >
+      //         <i className="pencil icon"></i>
+      //         Resume
               
-            </Link>   
+      //       </Link>   
 
-            <Menu.Item>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href='https://github.com/wgeorge1990'>
-                <Button
-                  circular
-                  color="purple"
-                  icon="github"
-                />
-              </a>
-            </Menu.Item>
+      //       <Menu.Item>
+      //         <a
+      //           target="_blank"
+      //           rel="noopener noreferrer"
+      //           href='https://github.com/wgeorge1990'>
+      //           <Button
+      //             circular
+      //             color="purple"
+      //             icon="github"
+      //           />
+      //         </a>
+      //       </Menu.Item>
 
-            <Menu.Item>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href='https://www.linkedin.com/in/wgdevelopment/'>
-                <Button
-                  circular
-                  color="linkedin"
-                  icon="linkedin"
-                />
-              </a>
-            </Menu.Item>
+      //       <Menu.Item>
+      //         <a
+      //           target="_blank"
+      //           rel="noopener noreferrer"
+      //           href='https://www.linkedin.com/in/wgdevelopment/'>
+      //           <Button
+      //             circular
+      //             color="linkedin"
+      //             icon="linkedin"
+      //           />
+      //         </a>
+      //       </Menu.Item>
 
-            <Menu.Item>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href='https://medium.com/@wgeorgedev'>
-                <Button
-                  circular
-                  color="grey"
-                  icon="medium"
-                />
-              </a>
-            </Menu.Item>
+      //       <Menu.Item>
+      //         <a
+      //           target="_blank"
+      //           rel="noopener noreferrer"
+      //           href='https://medium.com/@wgeorgedev'>
+      //           <Button
+      //             circular
+      //             color="grey"
+      //             icon="medium"
+      //           />
+      //         </a>
+      //       </Menu.Item>
             
-            <Menu.Item>
-              <a
-                href="mailto:wgeorgedev@gmail.com">
-                <Button
-                  circular
-                  color="google plus"
-                  icon="mail"
-                />
-              </a>
-            </Menu.Item>
+      //       <Menu.Item>
+      //         <a
+      //           href="mailto:wgeorgedev@gmail.com">
+      //           <Button
+      //             circular
+      //             color="google plus"
+      //             icon="mail"
+      //           />
+      //         </a>
+      //       </Menu.Item>
            
-            <Menu.Item>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://twitter.com/WGeorgeDev">
-                <Button
-                  circular
-                  color="twitter"
-                  icon="twitter"
-                  />
-                </a>
-            </Menu.Item>
-          </Sidebar>
+      //       <Menu.Item>
+      //         <a
+      //           target="_blank"
+      //           rel="noopener noreferrer"
+      //           href="https://twitter.com/WGeorgeDev">
+      //           <Button
+      //             circular
+      //             color="twitter"
+      //             icon="twitter"
+      //             />
+      //           </a>
+      //       </Menu.Item>
+      //     </Sidebar>
           
-          <Sidebar.Pusher dimmed={dimmed && visible} >
-            <Grid>
-              {/* <Grid.Row stretched> */}
-                {/* <Grid.Column width={1} style={{}}>
-                </Grid.Column> */}
-                {/* <Grid.Column width={16}> */}
-                  <Switch>
-                    <Route exact path='/' render={() => <Home />} />
-                    <Route exact path='/FineArt' render={() => <FineArt />} />
-                    <Route exact path='/Development' render={() => <Development />} />
-                    {/* <Route exact path='/Blogs' render={() => <Blogs />} /> */}
-                    <Route exact path='/Resume' render={() => <Resume />} />
-                  </Switch> 
-                {/* </Grid.Column> */}
-                {/* <Grid.Column width={1} style={{ }}> 
+      //     <Sidebar.Pusher dimmed={dimmed && visible} >
+      //       <Grid>
+      //         {/* <Grid.Row stretched> */}
+      //           {/* <Grid.Column width={1} style={{}}>
+      //           </Grid.Column> */}
+      //           {/* <Grid.Column width={16}> */}
+      //             <Switch>
+      //               <Route exact path='/' render={() => <Home />} />
+      //               <Route exact path='/FineArt' render={() => <FineArt />} />
+      //               <Route exact path='/Development' render={() => <Development />} />
+      //               {/* <Route exact path='/Blogs' render={() => <Blogs />} /> */}
+      //               <Route exact path='/Resume' render={() => <Resume />} />
+      //             </Switch> 
+      //           {/* </Grid.Column> */}
+      //           {/* <Grid.Column width={1} style={{ }}> 
 
-                </Grid.Column> */}
-              {/* </Grid.Row> */}
-          </Grid>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Container>
+      //           </Grid.Column> */}
+      //         {/* </Grid.Row> */}
+      //     </Grid>
+      //     </Sidebar.Pusher>
+      //   </Sidebar.Pushable>
+      // </Container>
     )
   };
 }
